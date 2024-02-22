aea8b3782a272ff61e95b75b5b9ecff7a47d1cd1
Nehir0109 <nehir.cakilci@gmail.com>
2024-02-22 23:19:09 +0300

    Haftanın Günleri

--- src/Main.java
+++ src/Main.java
@@ -47,7 +47,7 @@ import java.util.Scanner;
 }*/
 
 //Dört işlem hesaplayıcı
-public class Main{
+/*public class Main{
     public static void main(String[] args){
         Scanner get= new Scanner(System.in);
 
@@ -70,4 +70,42 @@ public class Main{
         System.out.printf("%f %s %f = %f",num1, islem, num2, sonuc);
 
     }
+}*/
+
+//Haftanın Günleri
+public class Main{
+    public static void main(String[] args){
+        Scanner get = new Scanner(System.in);
+
+        System.out.println("1 ile 7 arasında bir sayı giriniz: ");
+        Integer sayi= get.nextInt();
+
+
+        switch(sayi) {
+            case 1:
+                System.out.println("Pazartesi");
+                break;
+            case 2:
+                System.out.println("Salı");
+                break;
+            case 3:
+                System.out.println("Çarşamba");
+                break;
+            case 4:
+                System.out.println("Perşembe");
+                break;
+            case 5:
+                System.out.println("Cuma");
+                break;
+            case 6:
+                System.out.println("Cumartesi");
+                break;
+            case 7:
+                System.out.println("Pazar");
+                break;
+        }
+
+
+    }
+
 }
