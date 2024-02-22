262bbd87ef8bf89e2c9a638f5ce7631614fff94b
Nehir0109 <nehir.cakilci@gmail.com>
2024-02-22 11:17:23 +0300

    Telefon Numarası Biçimlendirme

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+// Telefon numarası biçimlendirme:
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner get = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("On haneli telefon numaranızı girin: ");
+        String telefonNo= get.nextLine();
+
+        if( telefonNo.length() == 10) {
+            String yeniNo = "(" + telefonNo.substring(0,3) + ")" + telefonNo.substring(3,6) + "-" + telefonNo.substring(6,10);
+            System.out.println("Biçimlendirilmiş no: "+ yeniNo);
+        }else {
+            System.out.println("Hata! Lüten 10 haneli bir telefon numarası girin.");
         }
+
     }
 }
